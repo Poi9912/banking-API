764b021f63ba716f2e8e66ac6cf637dbfd18b30c
Julio Urrego <jut9912@gmail.com>
2024-03-19 22:14:01 -0500

    documentation and disable https to only http and serve via proxy

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,6 @@
 FROM node:alpine
 WORKDIR /api
 COPY . .
-RUN apk add openssl
-#
-#RUN openssl req -x509 -out ./ssl/localhost.crt -keyout ./ssl/localhost.key -newkey rsa:2048 -nodes -sha256 -subj '/CN=localhost' -extensions EXT -config <(printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
-#RUN openssl genrsa -out localhost-key.pem 2048
-#RUN openssl req -new -x509 -sha256 -key localhost-key.pem -out localhost.pem -days 365 -subj '/CN=localhost' -extensions EXT -config <(printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
 RUN npm install
 EXPOSE 3000
 CMD ["npm","start"]
--- index.js
+++ index.js
@@ -1,30 +1,68 @@
 const express = require('express')
+const redoc = require('redoc-express')
 const app = express()
+const verMount = express()
 const apiKey = process.env.API_KEY
 //const http = require('http')
-const https = require('https')
-const fs = require('fs')
 const port = process.env.PORT || 3000
-
-const credentials = {
-    key: fs.readFileSync(__dirname + '/ssl/localhost.key'),
-    cert: fs.readFileSync(__dirname + '/ssl/localhost.crt')
-}
 
 //routers for the 2 different clients
 const customer = require('./routers/customer')
 const finance = require('./routers/finance')
 
+app.get('/banking/v1/docs/swagger.json', (req, res) => {
+    res.sendFile('swagger.json', { root: './docs'})
+})
+
+
 app.use('/customer',customer)
+console.log('Router Customer mounted')
+
 app.use('/finance',finance)
+console.log('Router Finance mounted')
 
 app.get('/', (req, res) => {
-    console.log('API')
-    res.send('API')
+    res.sendFile('index.html',{ root: './static'})
 })
 
-const server = https.createServer(credentials, app)
+app.get('/docs/swagger.json', (req, res) => {
+    res.sendFile('swagger.json', { root: './docs' });
+  });
+  
 
-server.listen(port, () => {
-    console.log('Express listening over HTTPS on port: ' + port)
-})
+app.get(
+    '/docs',
+    redoc({
+        title: 'API Docs',
+        specUrl: 'swagger.json',
+        nonce: '', // <= it is optional,we can omit this key and value
+        // we are now start supporting the redocOptions object
+        // you can omit the options object if you don't need it
+        // https://redocly.com/docs/api-reference-docs/configuration/functionality/
+        redocOptions: {
+            theme: {
+                colors: {
+                    primary: {
+                        main: '#6EC5AB'
+                    }
+                },
+                typography: {
+                    fontFamily: `"museo-sans", 'Helvetica Neue', Helvetica, Arial, sans-serif`,
+                    fontSize: '15px',
+                    lineHeight: '1.5',
+                    code: {
+                        code: '#87E8C7',
+                        backgroundColor: '#4D4D4E'
+                    }
+                },
+                menu: {
+                    backgroundColor: '#ffffff'
+                }
+            }
+        }
+    })
+);
+
+verMount.use('/banking/v1',app)
+
+verMount.listen(port)
